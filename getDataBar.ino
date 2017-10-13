91889a25be304eb7f487a43878db80ae4284c2dc
Raziel <robroblemol>
2017-10-13 12:19:05 -0500

    remove test archive

--- getDataBar.ino
+++
@@ -1,8 +0,0 @@
-  long getDayBar(int bar[barlength]){ 
-    Serial.println(int(barToInt(bar[7]))*100,DEC);
-    Serial.println(int(barToInt(bar[8]))*10,DEC);
-    Serial.println(int(barToInt(bar[9])),DEC);
-  //return ((bar[7]*100)+(bar[8]*10)+bar[9]);
-  return ((int(barToInt(bar[7]))*100)+(int(barToInt(bar[8]))*10)+int(barToInt(bar[9])));
-  }
-
